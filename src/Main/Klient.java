6377424ae2552cf933da41fb4aff2cf2b7e5fadc
shiftel <marcel.mal3cki@gmail.com>
2023-12-16 03:11:56 +0100

    Przeniesienie kilka do maina

--- src/Main/Klient.java
+++ src/Main/Klient.java
@@ -1,3 +1,5 @@
+package Main;
+
 import java.text.DecimalFormat;
 import java.util.List;
 
---
+++ src/Main/Produkt.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class Produkt {
+    private double cena;
+
+    public Produkt(double cena) {
+        this.cena = cena;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+}
