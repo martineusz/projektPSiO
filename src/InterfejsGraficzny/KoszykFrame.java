4e9f5e11ba1e6a8c82cfc189dc78475401591eb2
MateuszPiechowski <mateusz.piechowski@interia.pl>
2024-01-04 18:34:31 +0100

    młode zmiany w młodym kodzie KoszykFrame.class

--- src/InterfejsGraficzny/KoszykFrame.java
+++ src/InterfejsGraficzny/KoszykFrame.java
@@ -12,22 +12,133 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class KoszykFrame extends JFrame implements ActionListener {
+    double sumaCen;
+    JLabel labelSumaCen;
+    JPanel panelPodsumowanie;
     Koszyk koszyk;
     JPanel panelKoszyk;
+    JPanel panelDostawa;
     Map<JButton, Produkt> buttonProduktMap;
     JButton buttonZamowienie;
+    JButton buttonDostawa;
+    JCheckBox boxPaczkomat;
+    JCheckBox boxKurier;
+    JPanel panelGlowny;
+    JScrollPane scrollPane;
+    //text filedy
+    JTextField textImie;
+    JTextField textNazwisko;
+    JTextField textUlica;
+    JTextField textNumerDomu;
+    JTextField textKodPocztowy;
+    JTextField textMiejscowosc;
+    JTextField textKraj;
+    JTextField textNumerTelefonu;
+    JTextField textEmail;
 
-    KoszykFrame(Koszyk koszyk){
+    JLabel labelImie;
+    JLabel labelNazwisko;
+    JLabel labelUlica;
+    JLabel labelNumerDomu;
+    JLabel labelKodPocztowy;
+    JLabel labelMiejscowosc;
+    JLabel labelKraj;
+    JLabel labelNumerTelefonu;
+    JLabel labelEmail;
+
+    KoszykFrame(Koszyk koszyk) {
         this.koszyk = koszyk;
         JPanel panelGorny = new JPanel();
-        JPanel panelGlowny = new JPanel();
+        panelGlowny = new JPanel();
         JPanel panelDolny = new JPanel();
         panelKoszyk = new JPanel();
-        JPanel panelPodsumowanie = new JPanel();
+        panelDostawa = new JPanel();
+        panelPodsumowanie = new JPanel();
         buttonProduktMap = new HashMap<>();
         ImageIcon koszykImage = new ImageIcon("src/Obrazki/koszyk.png");
 
+//        TEXT FIELDY
+        textImie = new JTextField();
+        textImie.setPreferredSize(new Dimension(250,40));
+        textImie.setBounds(10,100,250,40);
+        panelDostawa.add(textImie);
+        labelImie = new JLabel();
+        labelImie.setBounds(10,85,50,10);
+        labelImie.setText("IMIĘ");
+        panelDostawa.add(labelImie);
 
+
+        textNazwisko = new JTextField();
+        textNazwisko.setPreferredSize(new Dimension(250,40));
+        textNazwisko.setBounds(10,175,250,40);
+        panelDostawa.add(textNazwisko);
+        labelNazwisko = new JLabel();
+        labelNazwisko.setBounds(10,160,150,10);
+        labelNazwisko.setText("NAZWISKO");
+        panelDostawa.add(labelNazwisko);
+
+        textUlica = new JTextField();
+        textUlica.setPreferredSize(new Dimension(250,40));
+        textUlica.setBounds(10,250,150,40);
+        panelDostawa.add(textUlica);
+        labelUlica = new JLabel();
+        labelUlica.setBounds(10,235,50,10);
+        labelUlica.setText("ULICA");
+        panelDostawa.add(labelUlica);
+
+        textNumerDomu = new JTextField();
+        textNumerDomu.setPreferredSize(new Dimension(250,40));
+        textNumerDomu.setBounds(180,250,80,40);
+        panelDostawa.add(textNumerDomu);
+        labelNumerDomu = new JLabel();
+        labelNumerDomu.setBounds(195,235,150,10);
+        labelNumerDomu.setText("NR. DOMU");
+        panelDostawa.add(labelNumerDomu);
+
+        textKodPocztowy = new JTextField();
+        textKodPocztowy.setPreferredSize(new Dimension(250,40));
+        textKodPocztowy.setBounds(10,325,80,40);
+        panelDostawa.add(textKodPocztowy);
+        labelKodPocztowy = new JLabel();
+        labelKodPocztowy.setBounds(10,310,150,10);
+        labelKodPocztowy.setText("KOD POCZTOWY");
+        panelDostawa.add(labelKodPocztowy);
+
+        textMiejscowosc = new JTextField();
+        textMiejscowosc.setPreferredSize(new Dimension(250,40));
+        textMiejscowosc.setBounds(110,325,150,40);
+        panelDostawa.add(textMiejscowosc);
+        labelMiejscowosc = new JLabel();
+        labelMiejscowosc.setBounds(170,310,150,10);
+        labelMiejscowosc.setText("MIEJSCOWOŚĆ");
+        panelDostawa.add(labelMiejscowosc);
+
+        textKraj = new JTextField();
+        textKraj.setPreferredSize(new Dimension(250,40));
+        textKraj.setBounds(280,100,250,40);
+        panelDostawa.add(textKraj);
+        labelKraj = new JLabel();
+        labelKraj.setBounds(280,85,50,10);
+        labelKraj.setText("KRAJ");
+        panelDostawa.add(labelKraj);
+
+        textNumerTelefonu = new JTextField();
+        textNumerTelefonu.setPreferredSize(new Dimension(250,40));
+        textNumerTelefonu.setBounds(280,175,250,40);
+        panelDostawa.add(textNumerTelefonu);
+        labelNumerTelefonu = new JLabel();
+        labelNumerTelefonu.setBounds(280,160,150,10);
+        labelNumerTelefonu.setText("NR. TELEFONU");
+        panelDostawa.add(labelNumerTelefonu);
+
+        textEmail = new JTextField();
+        textEmail.setPreferredSize(new Dimension(250,40));
+        textEmail.setBounds(280,250,250,40);
+        panelDostawa.add(textEmail);
+        labelEmail = new JLabel();
+        labelEmail.setBounds(280,235,50,10);
+        labelEmail.setText("EMAIL");
+        panelDostawa.add(labelEmail);
 
         //MOJ KOSZYK label
         JLabel mojKoszyk = new JLabel();
@@ -50,6 +161,7 @@ public class KoszykFrame extends JFrame implements ActionListener {
 
         panelGlowny.add(mojKoszyk);
         panelGlowny.add(panelKoszyk);
+        panelGlowny.add(panelDostawa);
         panelGlowny.add(panelPodsumowanie);
 
 
@@ -86,19 +198,31 @@ public class KoszykFrame extends JFrame implements ActionListener {
 
         //PANEL KOSZYK
         panelKoszyk.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panelKoszyk.setPreferredSize(new Dimension(600, panelKoszyk.getComponentCount() * 100));
+        panelKoszyk.setPreferredSize(new Dimension(600, panelKoszyk.getComponentCount() * 110));
 
-        JScrollPane scrollPane = new JScrollPane(panelKoszyk);
+        scrollPane = new JScrollPane(panelKoszyk);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setBounds(100, 105, 600, 450);
         panelGlowny.add(scrollPane);
+
+        //PANEL DOSTAWA
+        panelDostawa.setBorder(BorderFactory.createLineBorder(Color.red));
+        panelDostawa.setSize(new Dimension(600, 450));
+        panelDostawa.setBounds(100,105,600, 450);
+        panelDostawa.setLayout(null);
+        panelDostawa.setVisible(false);
+        panelGlowny.add(panelDostawa);
+
+
+
 
         //PANEL PODSUMOWANIE
         panelPodsumowanie.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         panelPodsumowanie.setBounds(1000,105,400,450);
         panelPodsumowanie.setLayout(null);
 
+        //BUTTON PODSUMOWANIE
         buttonZamowienie = new JButton();
         buttonZamowienie.setText("ZŁÓŻ ZAMÓWIENIE");
         buttonZamowienie.setPreferredSize(new Dimension(200,50));
@@ -106,6 +230,52 @@ public class KoszykFrame extends JFrame implements ActionListener {
         buttonZamowienie.addActionListener(this);
         //buttonZamowienie.setFont(new Font(null));
         panelPodsumowanie.add(buttonZamowienie);
+
+        //BUTTON DOSTAWA
+        buttonDostawa = new JButton();
+        buttonDostawa.setText("FINALIZUJ DOSTAWE");
+        buttonDostawa.setPreferredSize(new Dimension(200,50));
+        buttonDostawa.setBounds(0,300,400,80);
+        buttonDostawa.addActionListener(this);
+        //buttonZamowienie.setFont(new Font(null));
+        panelPodsumowanie.add(buttonDostawa);
+        buttonDostawa.setVisible(false);
+
+
+// CHECKBOX PACZKOMAT
+        boxPaczkomat = new JCheckBox("Paczkomat");
+        boxPaczkomat.setBounds(20, 20, 100, 20);
+        boxPaczkomat.setFocusable(true);
+        panelDostawa.add(boxPaczkomat);
+        boxPaczkomat.addActionListener(this);
+        boxPaczkomat.setSelected(true);
+        boxPaczkomat.setEnabled(false);
+// CHECKBOX KURIER
+        boxKurier = new JCheckBox("Kurier");
+        boxKurier.setBounds(20, 50, 100, 20);
+        boxKurier.setFocusable(true);
+        panelDostawa.add(boxKurier);
+        boxKurier.addActionListener(this);
+
+        //PODSUMOWANIE LABEL
+        JLabel labelPodsumowanie = new JLabel();
+        labelPodsumowanie.setText("PODSUMOWANIE");
+        labelPodsumowanie.setBounds(25,0,200,100);
+        labelPodsumowanie.setFont(new Font(null, Font.BOLD, 20));
+        panelPodsumowanie.add(labelPodsumowanie);
+
+        //SUMA CEN PRODUKTOW Z KOSZYKA LABELLLL
+        labelSumaCen = new JLabel();
+        sumaCen = 0;
+
+        for (int i = 0; i < koszyk.getListaProduktow().size(); i++) {
+            sumaCen+=koszyk.getListaProduktow().get(i).getCena();
+        }
+
+        labelSumaCen.setText("SUMA: " + sumaCen);
+        labelSumaCen.setBounds(25,50,200,100);
+        labelSumaCen.setFont(new Font(null, Font.BOLD, 20));
+        panelPodsumowanie.add(labelSumaCen);
 
 
 
@@ -125,21 +295,54 @@ public class KoszykFrame extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JButton sourceButton = (JButton) e.getSource();
-        Produkt produktToRemove = buttonProduktMap.get(sourceButton);
-
-        if (produktToRemove != null) {
-            koszyk.getListaProduktow().remove(produktToRemove);
-            panelKoszyk.remove(sourceButton.getParent()); // Usunięcie całego panelu produktu
-            panelKoszyk.revalidate();
-            panelKoszyk.repaint();
+        if (e.getSource() == boxPaczkomat) {
+            if (boxPaczkomat.isSelected()) {
+                boxKurier.setSelected(false); // Jeśli paczkomat jest zaznaczony, odznacz kuriera
+                boxKurier.setFocusable(true);
+                boxKurier.setEnabled(true);
+                boxPaczkomat.setFocusable(false);
+                boxPaczkomat.setEnabled(false);
+                sumaCen += 4;
+                labelSumaCen.setText(String.valueOf("SUMA: " + sumaCen));
+            }
+        } else if (e.getSource() == boxKurier) {
+            if (boxKurier.isSelected()) {
+                boxPaczkomat.setSelected(false); // Jeśli kurier jest zaznaczony, odznacz paczkomat
+                boxPaczkomat.setFocusable(true);
+                boxPaczkomat.setEnabled(true);
+                boxKurier.setFocusable(false);
+                boxKurier.setEnabled(false);
+                sumaCen -= 4;
+                labelSumaCen.setText(String.valueOf("SUMA: " + sumaCen));
+            }
         }
-        if(e.getSource() == buttonZamowienie) {
-//            panelKoszyk.removeAll();
-            panelKoszyk.revalidate();
-            panelKoszyk.repaint();
-        }
 
+        if(e.getSource() instanceof JButton){
+            JButton sourceButton = (JButton) e.getSource();
+            Produkt produktToRemove = buttonProduktMap.get(sourceButton);
+
+            if (produktToRemove != null) {
+                sumaCen -= produktToRemove.getCena();
+                koszyk.getListaProduktow().remove(produktToRemove);
+                panelKoszyk.remove(sourceButton.getParent()); // Usunięcie całego panelu produktu
+                panelKoszyk.revalidate();
+                panelKoszyk.repaint();
+                labelSumaCen.setText(String.valueOf("SUMA: " + sumaCen));
+            }
+            if(e.getSource() == buttonZamowienie) {
+                panelKoszyk.setVisible(false);
+                buttonDostawa.setVisible(true);
+                panelKoszyk.revalidate();
+                panelKoszyk.repaint();
+                panelDostawa.setVisible(true);
+                panelDostawa.revalidate();
+                panelDostawa.repaint();
+                buttonZamowienie.setVisible(false);
+                scrollPane.setVisible(false);
+                sumaCen += 15.99;
+                labelSumaCen.setText(String.valueOf("SUMA: " + sumaCen));
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -151,18 +354,17 @@ public class KoszykFrame extends JFrame implements ActionListener {
         sklep.zalogowanyKlient.getKoszyk().dodajProdukt(sklep.getListaProduktow().get(0));
         sklep.zalogowanyKlient.getKoszyk().dodajProdukt(sklep.getListaProduktow().get(1));
 
-            if (sklep.zalogowanyKlient.getKoszyk().getListaProduktow().size() == 0) {
-                System.out.println("PUSTY KOSZYK");
-            } else {
-                for (int i = 0; i < sklep.zalogowanyKlient.getKoszyk().getListaProduktow().size(); i++) {
-                    System.out.println((i+1) + ".");
-                    System.out.println(sklep.zalogowanyKlient.getKoszyk().getListaProduktow().get(i).toString());
-                }
+        if (sklep.zalogowanyKlient.getKoszyk().getListaProduktow().size() == 0) {
+            System.out.println("PUSTY KOSZYK");
+        } else {
+            for (int i = 0; i < sklep.zalogowanyKlient.getKoszyk().getListaProduktow().size(); i++) {
+                System.out.println((i+1) + ".");
+                System.out.println(sklep.zalogowanyKlient.getKoszyk().getListaProduktow().get(i).toString());
             }
+        }
 
         new KoszykFrame(sklep.zalogowanyKlient.getKoszyk());
     }
 }
-
 
 
